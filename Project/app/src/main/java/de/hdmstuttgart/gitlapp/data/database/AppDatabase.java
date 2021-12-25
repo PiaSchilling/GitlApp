83ead57576355676b0c29215018d92dc77af5b1c
Pia Schilling <ps149@hdm-stuttgart.de>
2021-12-25 21:31:34 +0100

    feat(data): add app database class for room implementation
    Implements singleton access for the room database object

---
+++ Project/app/src/main/java/de/hdmstuttgart/gitlapp/data/database/AppDatabase.java
@@ -0,0 +1,39 @@
+package de.hdmstuttgart.gitlapp.data.database;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import de.hdmstuttgart.gitlapp.models.Issue;
+import de.hdmstuttgart.gitlapp.models.Label;
+import de.hdmstuttgart.gitlapp.models.Milestone;
+import de.hdmstuttgart.gitlapp.models.Note;
+import de.hdmstuttgart.gitlapp.models.Profile;
+import de.hdmstuttgart.gitlapp.models.Project;
+import de.hdmstuttgart.gitlapp.models.User;
+
+@Database(entities = {Issue.class, Label.class, Milestone.class, Note.class, Profile.class, Project.class, User.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+
+    private static AppDatabase appDatabase;
+
+    private AppDatabase(){}
+
+    //Singleton access bc each db object is expensive (its recommended to implement this as a singleton on android.developers.com)
+    public static AppDatabase getDatabaseInstance(Context context) {
+        if(appDatabase == null){
+            appDatabase = Room.databaseBuilder(context,AppDatabase.class,"app-database").build();
+        }
+       return appDatabase;
+    }
+
+    public abstract IssueDao issueDao();
+    public abstract LabelDao labelDao();
+    public abstract MilestoneDao milestoneDao();
+    public abstract NoteDao noteDao();
+    public abstract ProfileDao profileDao();
+    public abstract ProjectDao projectDao();
+    public abstract UserDao userDao();
+}
