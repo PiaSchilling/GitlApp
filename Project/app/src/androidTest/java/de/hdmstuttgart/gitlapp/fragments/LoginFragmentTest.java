2be8a69980d4e8dc6f3cc3a954b5e0812b95b4df
Pia Schilling <ps149@hdm-stuttgart.de>
2022-01-27 22:23:06 +0100

    test: add login fragment test
    hardcoded access token included, this is not a pretty solution but better than nothing for now
    goal is to mock the profile repo somehow

---
+++ Project/app/src/androidTest/java/de/hdmstuttgart/gitlapp/fragments/LoginFragmentTest.java
@@ -0,0 +1,72 @@
+package de.hdmstuttgart.gitlapp.fragments;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.*;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import de.hdmstuttgart.gitlapp.MainActivity;
+import de.hdmstuttgart.gitlapp.R;
+
+public class LoginFragmentTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityScenarioRule
+            = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void testLoginFragment() {
+
+        onView(withId(R.id.gitlab_title)).check(matches(isDisplayed()));
+
+
+        onView(withId(R.id.openUrl_button))
+                .perform(click()); //should not open browser when host url is not set
+
+        onView(withId(R.id.login_button))
+                .perform(click()); //should not login with empty fields
+
+        onView(withId(R.id.hostUrl_textField))
+                .perform(click());
+
+        onView(withId(R.id.hostUrl_editText))
+                .perform(typeText("https://gitlab.mi.hdm-stuttgart.de"), closeSoftKeyboard());
+
+        onView(withId(R.id.userId_editText))
+                .perform(click());
+
+        onView(withId(R.id.userId_editText))
+                .perform(typeText("2233"), closeSoftKeyboard());
+
+        onView(withId(R.id.accessToken_editText))
+                .perform(click());
+
+        onView(withId(R.id.accessToken_editText))
+                .perform(typeText("glpat-im7xUxYLmQv1LnKnvesr"), closeSoftKeyboard());
+
+        onView(withId(R.id.login_button))
+                .perform(click()); //should now login
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        onView(withId(R.id.userNameLabel)).check(matches(withText("Schilling Pia")));
+
+
+    }
+}
